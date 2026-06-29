bfa13ddd8e30878708203be2a9493d4f9c6a1e9e
Anthony <asadbeksaydullayev2911@gmail.com>
2026-06-29 19:03:03 +0900

    feat: organize homePage releated section components

--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -1,5 +1,21 @@
+import React from "react";
 import { Container } from "@mui/material";
+import Statistics from "./Statistics";
+import PopularDishes from "./PopularDishes";
+import NewDishes from "./NewDishes";
+import Advertiesment from "./Advertiesment";
+import ActiveUsers from "./ActiveUsers";
+import Events from "./Events";
 
 export default function HomePage() {
-  return <Container>HomePage</Container>;
+  return (
+    <div>
+      <Statistics />
+      <PopularDishes />
+      <NewDishes />
+      <Advertiesment />
+      <ActiveUsers />
+      <Events />
+    </div>
+  )
 }
