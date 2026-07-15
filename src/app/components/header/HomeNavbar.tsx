08b42d1aef46a9c8122afcbfca2d5191bcd6db64
Anthony <asadbeksaydullayev2911@gmail.com>
2026-07-15 22:05:17 +0900

    feat: practice useEffect & useState Hooks

--- src/app/components/header/HomeNavbar.tsx
+++ src/app/components/header/HomeNavbar.tsx
@@ -1,9 +1,30 @@
 import { Box, Button, Container, Stack } from "@mui/material";
 import { NavLink } from "react-router-dom";
 import Basket from "./Basket";
+import { useEffect, useState } from "react";
 
 export default function HomeNavbar() {
-    const authMember = true
+    const authMember = null;
+    const [count, setCount] = useState<number>(0);
+    const [value, setValue] = useState<boolean>(true);
+
+
+    useEffect(() => {
+        console.log("componentDidMount"); // DATA FETCH
+        setCount(count + 1);
+
+        return () => {
+            console.log("componentWillUnmount")
+        };
+
+    }, [value]);
+
+
+    /** HANDLERS **/
+
+    const buttonHandler = () => {
+        setValue(!value);
+    }
 
     return (
         <div className="home-navbar">
@@ -64,12 +85,12 @@ export default function HomeNavbar() {
                             The Choice, not just a choice
                         </Box>
                         <Box className="service-txt">
-                            24 hours service
+                            {count} hours service
                         </Box>
 
                         <Box className={"signup"}>
                             {!authMember ? (
-                                <Button variant="contained" className="signup-button">SIGN UP</Button>
+                                <Button variant="contained" className="signup-button" onClick={buttonHandler}>SIGN UP</Button>
                             ) : null}
                         </Box>
 
