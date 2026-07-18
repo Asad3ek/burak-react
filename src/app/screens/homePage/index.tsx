2ede89e05dbe9414c61b51075874bded6c5fc0b2
Anthony <asadbeksaydullayev2911@gmail.com>
2026-07-18 18:16:38 +0900

    feat: create HomePage Redux configuration

--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Container } from "@mui/material";
 import Statistics from "./Statistics";
 import PopularDishes from "./PopularDishes";
@@ -10,6 +10,17 @@ import "../../../css/home.css"
 
 
 export default function HomePage() {
+
+  //SELECTOR: Store => Data
+
+  useEffect(() => {
+    //Backend Server data request => Data
+
+    //SLICE: Data => Store 
+  }, []);
+
+
+
   return (
     <div className="homePage">
       <Statistics />
--- src/app/store.ts
+++ src/app/store.ts
@@ -1,7 +1,11 @@
 import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
+import { homedir } from "os";
+import HomePageReducer from "./screens/homePage/slice";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    homePage: HomePageReducer
+  },
 });
 
 export type AppDispatch = typeof store.dispatch;
